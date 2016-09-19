5fa7f8f446d0cbe8e5c358fdd75e09d0207f5b49
Geordon Worley <vadixidav@gmail.com>
2016-09-19 19:22:32 -0400

    add network code

---
+++ recv.c
@@ -0,0 +1,68 @@
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+#include "network_recv.h"
+#include "processing.h"
+#include "debug.h"
+
+#define NETWORK_RECV_QUEUE_LEN 14
+
+QueueHandle_t network_recv_queue;
+
+bool network_recv_add_buffer_from_isr(CharBuffer *buffer) {
+    BaseType_t higher_priority_task_woken = pdFALSE;
+    // Attempt add the buffer from the isr to the queue.
+    if (xQueueSendToBackFromISR(network_recv_queue, buffer, &higher_priority_task_woken)) {
+        // If a higher priority task was waiting for something on the queue, switch to it.
+        portEND_SWITCHING_ISR(higher_priority_task_woken);
+        return true;
+    // We didn't receive a buffer.
+    } else {
+        // Indicate on LD4 that we lost a packet.
+        // NOTE: LD4 conflicts with SDA2 (I2C).
+        SYS_PORTS_PinWrite(0, PORT_CHANNEL_A, PORTS_BIT_POS_3, 1);
+        return false;
+    }
+    return false;
+}
+
+void network_recv_init() {
+    network_recv_queue = xQueueCreate(NETWORK_RECV_QUEUE_LEN, sizeof(CharBuffer));
+    wifly_int_init();
+}
+
+void network_recv_task() {
+    CharBuffer buffer;
+    NRMessage message;
+    while (1) {
+        xQueueReceive(network_recv_queue, &buffer, portMAX_DELAY);
+        // Parse the JSON into objects.
+        // TODO: Parse from JSON.
+        // Assume the object is a stat query.
+        message.type = NR_QUERY_STATS;
+        message.data.query_stats.dummy = 'd';
+        processing_add_recvmsg(&message);
+    }
+}
---
+++ send.c
@@ -0,0 +1,111 @@
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+#include "send.h"
+#include "int_wifly.h"
+#include "int_adc.h"
+#include "debug.h"
+#include <stdio.h>
+
+#define NETWORK_SEND_QUEUE_LEN 16
+#define MESSAGE_BUF_SIZE 512
+#define TOTAL_MESSAGE_BUFFS 4
+
+QueueHandle_t network_send_queue;
+
+char messagebuffs[TOTAL_MESSAGE_BUFFS][MESSAGE_BUF_SIZE];
+char *messagebuff;
+unsigned choose_buff;
+
+void network_send_add_message(NSMessage *message) {
+    xQueueSendToBack(network_send_queue, message, portMAX_DELAY);
+}
+
+void network_send_add_message_isr(NSMessage *message) {
+    BaseType_t higher_priority_task_woken = pdFALSE;
+    // Attempt add the buffer from the isr to the queue.
+    if (xQueueSendToBackFromISR(network_send_queue, message, &higher_priority_task_woken)) {
+        // If a higher priority task was waiting for something on the queue, switch to it.
+        portEND_SWITCHING_ISR(higher_priority_task_woken);
+    // We didn't receive a buffer.
+    } else {
+        // Indicate on LD4 that we lost a packet.
+        // NOTE: LD4 conflicts with SDA2 (I2C).
+        SYS_PORTS_PinWrite(0, PORT_CHANNEL_A, PORTS_BIT_POS_3, 1);
+    }
+}
+
+void next_messagebuff() {
+    messagebuff = messagebuffs[(choose_buff++) % TOTAL_MESSAGE_BUFFS];
+}
+
+void network_send_init() {
+    messagebuff = messagebuffs[0];
+    choose_buff = 0;
+    network_send_queue = xQueueCreate(NETWORK_SEND_QUEUE_LEN, sizeof(NSMessage));
+    int_adc_init();
+    DRV_ADC_Open();
+}
+
+void network_send_task() {
+    NSMessage message;
+    CharBuffer buffer;
+    while (1) {
+        xQueueReceive(network_send_queue, &message, portMAX_DELAY);
+        switch (message.type) {
+            case NS_NETSTATS: {
+                MSGNetstats *netstats = &message.data.netstats;
+                buffer.buff = messagebuff;
+                buffer.length = sprintf(messagebuff, "{\"Netstats\":{\"myName\":\"Sensor\",\"numGoodMessagesRecved\":%d,\"numCommErrors\":%d,\"numJSONRequestsRecved\":%d,\"numJSONResponsesRecved\":%d,\"numJSONRequestsSent\":%d,\"numJSONResponsesSent\":%d}}",
+                        netstats->numGoodMessagesRecved,
+                        netstats->numCommErrors,
+                        netstats->numJSONRequestsRecved,
+                        netstats->numJSONResponsesRecved,
+                        netstats->numJSONRequestsSent,
+                        netstats->numJSONResponsesSent);
+                
+                if (buffer.length > 0) {
+                    wifly_int_send_buffer(&buffer);
+                    next_messagebuff();
+                } else {
+                    SYS_PORTS_PinWrite(0, PORT_CHANNEL_A, PORTS_BIT_POS_3, 1);
+                }
+            } break;
+            case NS_ADC_READING: {
+                MSGAdcReading *adc_reading = &message.data.adc_reading;
+                buffer.buff = messagebuff;
+                buffer.length = sprintf(messagebuff, "{\"AdcReading\":{\"reading\":%d}}", adc_reading->reading);
+                
+                if (buffer.length > 0) {
+                    wifly_int_send_buffer(&buffer);
+                    next_messagebuff();
+                } else {
+                    SYS_PORTS_PinWrite(0, PORT_CHANNEL_A, PORTS_BIT_POS_3, 1);
+                }
+            } break;
+        }
+    }
+    
+}
